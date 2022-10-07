569e63188cd5ca354e03420461ecff792ece319c
Sathiya Lingam Senthil Kumar <sathiyalingam.senthilkumar@lumenradio.com>
2022-10-07 16:41:04 +0200

    nvim: Change a few color schemes for Gruvbox
    The colorschemes for nvim-tree is modified to match that of colorscheme
    of ls. The function and string colors are changed in this commit.

--- config/nvim/lua/plugin-settings/colors/gruvbox.lua
+++ config/nvim/lua/plugin-settings/colors/gruvbox.lua
@@ -6,6 +6,20 @@ end
 -- Gruvbox configuration
 vim.g.gruvbox_contrast_dark = 'hard'
 
+gb.setup({
+    overrides = {
+        -- Color overides
+        String = { fg =  "#99CCFF", italic = true },
+        Function = { fg =  "#FFFF66", bold = true },
+
+        -- nvim-tree colors (match ls output colors)
+        Directory = { fg = "#3399ff" },
+        NvimTreeExecFile = { fg = "#FF3333", bold = true },
+        NvimTreeSymlink = { fg = "#FF3399" },
+        NvimTreeFolderIcon = { fg = "#C0C0C0", bold = true },
+    }
+})
+
 vim.cmd([[
   color gruvbox
 ]])
